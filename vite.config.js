1beec8454e9ac83dc4e0844773d952589ee5232b
João Vitor <jvitor.arrudal@gmail.com>
2024-03-03 01:07:37 -0300

    git pages config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://joaao-vitor.github.io/fcc-calculator/'
 })
